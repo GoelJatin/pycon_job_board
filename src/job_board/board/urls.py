052f1e712a1f5ae4e3caf3a7a51a7f2095a97252
Jatin Goel <jatingoel1037@hotmail.com>
2020-09-25 16:58:21 +0530

    Add URLs for Board app

--- src/job_board/board/urls.py
+++ src/job_board/board/urls.py
@@ -6,4 +6,7 @@ from . import views
 app_name = 'web'
 
 urlpatterns = [
+    # path('', views.JobView.as_view(), name='view_jobs'),
+    # path('jobs', views.JobView.as_view(), name='view_jobs'),
+    path('submit_job', views.SubmitJobView.as_view(), name='submit_job')
 ]
