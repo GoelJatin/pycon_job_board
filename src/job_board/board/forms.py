9e1f3c6b3c1a8e6b38fe74ae3fdcc4d45603bada
Jatin Goel <jatingoel1037@hotmail.com>
2020-09-26 23:57:58 +0530

    Add more form validations

--- src/job_board/board/forms.py
+++ src/job_board/board/forms.py
@@ -25,54 +25,83 @@ class SubmitterForm(forms.ModelForm):
     otp = forms.IntegerField(min_value=OTP_MIN_VALUE, max_value=OTP_MAX_VALUE, required=False)
 
     def clean(self):
-        cleaned_data = super(SubmitterForm, self).clean()
+        email = self.data.get('email')
 
-        if 'email' in cleaned_data:
-            email = cleaned_data['email']
+        try:
+            # Don't raise any errors if cleanup is called for an existing submitter
+            _ = Submitter.objects.get(email=email)
+            self.cleaned_data['email'] = email
+        except Submitter.DoesNotExist:
+            cleaned_data = super(SubmitterForm, self).clean()
 
-            if 'send_otp' in self.data:
-                otp = random.randint(OTP_MIN_VALUE, OTP_MAX_VALUE)
-                user = User.objects.filter(email=email).first()
+            # do futher validations only if email is valid
+            if 'email' in cleaned_data:
+                email = cleaned_data['email']
 
-                if user is None:
-                    user = User.objects.create(email=email, code=otp)
+                if 'send_otp' in self.data:
+                    otp = random.randint(OTP_MIN_VALUE, OTP_MAX_VALUE)
+                    user, _ = User.objects.update_or_create(
+                        defaults={'code': otp},
+                        email=email
+                    )
+                    user.save()
+
+                    # TODO: send otp to email
+                    raise ValidationError('Please provide the OTP sent to your email')
                 else:
-                    user.code = otp
+                    user = User.objects.filter(email=email).first()
 
-                user.save()
-                raise ValidationError('Please provide the OTP sent to your email')
-            else:
-                user = User.objects.filter(email=email).first()
-
-                if cleaned_data['otp'] != user.code:
-                    raise ValidationError('OTP is invalid')
+                    if user is None or cleaned_data['otp'] != user.code:
+                        raise ValidationError('OTP is invalid')
 
     def save(self):
         del self.cleaned_data['otp']
-        submitter = Submitter.objects.create(**self.cleaned_data)
+
+        email = self.cleaned_data['email']
+        del self.cleaned_data['email']
+
+        print(self.cleaned_data, email)
+
+        submitter, _ = Submitter.objects.update_or_create(defaults=self.cleaned_data, email=email)
         submitter.save()
+
+        User.objects.filter(email=email).delete()
+        self.cleaned_data['email'] = email
 
 
 class JobForm(forms.ModelForm):
 
     class Meta:
         model = Job
-        exclude = ('submitter',)
+        exclude = ('submitted_by',)
+
+    def __init__(self, *args, **kwargs):
+        super(JobForm, self).__init__(*args, **kwargs)
+
+        self.fields['job_url'].required = False
+        self.fields['description'].required = False
 
     def clean(self):
         cleaned_data = super(JobForm, self).clean()
 
-        if 'website' in cleaned_data and 'job_url' in cleaned_data:
-            # Run the validation calls concurrently to save time
-            with ThreadPoolExecutor() as executor:
+        if cleaned_data.get('job_url') is None and cleaned_data.get('description') is None:
+            raise ValidationError('Either job url or description must be provided')
+
+        is_valid_website = is_valid_job_url = None
+
+        # Run the validation calls concurrently to save time
+        with ThreadPoolExecutor() as executor:
+            if 'website' in cleaned_data:
                 is_valid_website = executor.submit(is_valid_url, cleaned_data['website'])
+
+            if 'job_url' in cleaned_data and cleaned_data['job_url']:
                 is_valid_job_url = executor.submit(is_valid_url, cleaned_data['job_url'])
 
-            if not is_valid_website.result():
-                raise ValidationError('Website is not reachable, please provide a valid URL')
+        if is_valid_website and is_valid_website.result() is False:
+            raise ValidationError('Website is not reachable, please provide a valid URL')
 
-            if not is_valid_job_url.result():
-                raise ValidationError('Job URL is not reachable, please check and validate')
+        if is_valid_job_url and is_valid_job_url.result() is False:
+            raise ValidationError('Job URL is not reachable, please check and validate')
 
     def save(self):
         job = Job.objects.create(**self.cleaned_data)
